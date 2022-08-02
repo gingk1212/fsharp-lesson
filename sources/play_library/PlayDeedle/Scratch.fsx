4cfa4ef8911b5200766f364c6ce72f3f0ec26248
gingk <g.ingk.2927@gmail.com>
2022-08-03 00:23:43 +0900

    Fix 課題5

--- sources/play_library/PlayDeedle/Scratch.fsx
+++ sources/play_library/PlayDeedle/Scratch.fsx
@@ -14,11 +14,11 @@ df.RowsDense
 df.Columns.[ ["場所"; "学年"] ]
 
 let filter fn rows =
-    rows |> Series.filterValues(fn) |> Frame.ofRows
+    rows |> Series.filterValues fn |> Frame.ofRows
 
 df.RowsDense |> filter (fun row -> row.GetAs<string>("専門") = "数学")
 
-let project (frame: Frame<int, string>) (list: string list)  =
-    frame.Columns.[ list ]
+let project (frame: Frame<int, string>) (names: string list)  =
+    frame.Columns.[ names ]
 
 project df ["場所"; "学年"]
