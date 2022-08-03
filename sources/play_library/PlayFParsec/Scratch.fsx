75f5423f4edae69e018a0e51f4944b9f638da786
gingk <g.ingk.2927@gmail.com>
2022-08-03 20:44:47 +0900

    Take FParsec tutorial

---
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -0,0 +1,123 @@
+#r "nuget: FParsec"
+open FParsec
+
+run pfloat "1.25"
+
+// let ws = spaces
+// let float_ws = pfloat .>> ws
+// run float_ws "1.25 "
+
+// 4.2 Parsing a single float
+let test p str =
+    match run p str with
+    | Success(result, _, _) -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test pfloat "1.25"
+test pfloat " 1.25"
+test pfloat ""
+
+// 4.3 Parsing a float between brackets
+let str s = pstring s
+let floatBetweenBrackets = str "[" >>. pfloat .>> str "]"
+
+test floatBetweenBrackets "[1.0]"
+test floatBetweenBrackets ""
+
+// 4.4 Abstracting parsers
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+let floatBetweenBrackets2 = pfloat |> betweenStrings "[" "]"
+let floatBetweenDoubleBrackets = pfloat |> betweenStrings "[[" "]]"
+
+test floatBetweenBrackets2 "[1.25]"
+test floatBetweenDoubleBrackets "[[1.25]]"
+
+// let between pBegin pEnd p = pBegin >>. p .>> pEnd
+// let betweenStrings s1 s2 p = p |> between (str s1) (str s2)
+
+// 4.5 Parsing a list of floats
+test (many floatBetweenBrackets) ""
+test (many floatBetweenBrackets) "[1.0]"
+test (many floatBetweenBrackets) "[2][3][4]"
+test (many floatBetweenBrackets) "[1][2.0E][3]"
+test (many floatBetweenBrackets) "[2][][4]"
+test (many floatBetweenBrackets) "[2][3]a[4]"
+
+let floatList = str "[" >>. sepBy pfloat (str ",") .>> str "]"
+
+test floatList "[]"
+test floatList "[1,2,3]"
+test floatList "[1,2,3,]"
+
+// 4.6 Handling whitespace
+test floatList "[1.0, 2.0]"
+
+let ws = spaces
+let str_ws s = pstring s .>> ws
+let float_ws = pfloat .>> ws
+let numberList = str_ws "[" >>. sepBy float_ws (str_ws ",") .>> str_ws "]"
+
+test numberList "[ 1, 3, 33    , 3 ] "
+test numberList @"[ 1 , 
+                             2, 3 ] "
+test numberList @"[ 1,
+                         2; 3]"
+
+let numberListFile = ws >>. numberList .>> eof
+
+test numberListFile " [1, 2, 3] [4]"
+
+// 4.7 Parsing string data
+test (many (str "a" <|> str "b")) "abba"
+
+test (skipStringCI "<float>" >>. pfloat) "<FLOAT>1.0"
+
+let identifier =
+    let isIdentifierFirstChar c = isLetter c || c = '_'
+    let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+
+    many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"
+    .>> ws
+
+test identifier "_"
+test identifier "_test1="
+test identifier "1"
+
+let stringLiteral =
+    let normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
+    let unescape c = match c with
+                         | 'n' -> '\n'
+                         | 'r' -> '\r'
+                         | 't' -> '\t'
+                         | c   -> c
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
+    between (pstring "\"") (pstring "\"")
+            (manyChars (normalChar <|> escapedChar))
+
+test stringLiteral "\"abc\""
+test stringLiteral "\"abc\\\"def\\\\ghi\""
+test stringLiteral "\"abc\\def\""
+
+let stringLiteral2 =
+    let normalCharSnippet = many1Satisfy (fun c -> c <> '\\' && c <> '"')
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> function
+                                                            | 'n' -> "\n"
+                                                            | 'r' -> "\r"
+                                                            | 't' -> "\t"
+                                                            | c   -> string c)
+    between (pstring "\"") (pstring "\"")
+            (manyStrings (normalCharSnippet <|> escapedChar))
+
+test stringLiteral2 "\"abc\""
+
+let stringLiteral3 =
+    let normalCharSnippet = manySatisfy (fun c -> c <> '\\' && c <> '"')
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> function
+                                                            | 'n' -> "\n"
+                                                            | 'r' -> "\r"
+                                                            | 't' -> "\t"
+                                                            | c   -> string c)
+    between (pstring "\"") (pstring "\"")
+            (stringsSepBy normalCharSnippet escapedChar)
+
+test stringLiteral3 "\"abc\""
