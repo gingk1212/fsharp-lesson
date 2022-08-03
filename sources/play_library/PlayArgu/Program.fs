73c68a9151f96b753e7f5211a0588110e12a0300
gingk <g.ingk.2927@gmail.com>
2022-08-03 11:46:12 +0900

    課題6: オプションhelloを処理 2

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,8 +1,7 @@
-﻿open System
-
-let args = Environment.GetCommandLineArgs()
-
-if args.Length = 2 && args[1] = "-hello" then
-    printfn "Hello World"
-else
-    printfn "I don't know"
+﻿[<EntryPoint>]
+let main args =
+    if args.Length = 1 && args[0] = "-hello" then
+        printfn "Hello World"
+    else
+        printfn "I don't know"
+    0
