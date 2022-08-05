604407a527d4f1574617a07c6622ac41f089e986
gingk <g.ingk.2927@gmail.com>
2022-08-05 20:33:30 +0900

    Fix 課題0

--- sources/ToyRel/Scratch.fsx
+++ sources/ToyRel/Scratch.fsx
@@ -1,7 +1,7 @@
 open System.Text.RegularExpressions
 
 let firstIdentifier = "([_@a-zA-Z]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})"
-let identifier = "([-_@a-zA-Z0-9]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})+"
+let identifier = "([-_@a-zA-Z0-9]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})*"
 
 // 全体マッチする
 Regex.Match("abc", firstIdentifier + identifier)
@@ -11,6 +11,7 @@ Regex.Match("専門", firstIdentifier + identifier)
 Regex.Match("フロア", firstIdentifier + identifier)
 Regex.Match("@abc", firstIdentifier + identifier)
 Regex.Match("a-bc", firstIdentifier + identifier)
+Regex.Match("a", firstIdentifier + identifier)
 
 // 全体マッチしない
 Regex.Match("123", firstIdentifier + identifier)
