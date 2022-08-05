2e461be7f78bb99ae831abaabcb65b24b857d2ee
gingk <g.ingk.2927@gmail.com>
2022-08-05 19:21:57 +0900

    課題0: identifierにマッチする正規表現を書け

---
+++ sources/ToyRel/Scratch.fsx
@@ -0,0 +1,27 @@
+open System.Text.RegularExpressions
+
+let firstIdentifier = "([_@a-zA-Z]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})"
+let identifier = "([-_@a-zA-Z0-9]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})+"
+
+// 全体マッチする
+Regex.Match("abc", firstIdentifier + identifier)
+Regex.Match("_abc123", firstIdentifier + identifier)
+Regex.Match("abc_123", firstIdentifier + identifier)
+Regex.Match("専門", firstIdentifier + identifier)
+Regex.Match("フロア", firstIdentifier + identifier)
+Regex.Match("@abc", firstIdentifier + identifier)
+Regex.Match("a-bc", firstIdentifier + identifier)
+
+// 全体マッチしない
+Regex.Match("123", firstIdentifier + identifier)
+Regex.Match("abc.def", firstIdentifier + identifier)
+Regex.Match("abc*", firstIdentifier + identifier)
+Regex.Match("abc:def", firstIdentifier + identifier)
+Regex.Match("abc def", firstIdentifier + identifier)
+Regex.Match("(abc)", firstIdentifier + identifier)
+Regex.Match("abc+def", firstIdentifier + identifier)
+Regex.Match("-abc", firstIdentifier + identifier)
+
+
+
+
