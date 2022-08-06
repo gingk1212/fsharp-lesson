1909b7f9bb853fcb9362a3e5f41ea8fbbb62f5b0
gingk <g.ingk.2927@gmail.com>
2022-08-06 23:46:04 +0900

    Add comment

--- sources/ToyRel/Scratch.fsx
+++ sources/ToyRel/Scratch.fsx
@@ -12,6 +12,7 @@ let test p str =
     | ParserResult.Success(result, _, _) -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
+// 課題0: identifierにマッチする正規表現を書け
 let firstIdentifier = "([_@a-zA-Z]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})"
 let identifier = "([-_@a-zA-Z0-9]|\p{IsHiragana}|\p{IsKatakana}|\p{IsCJKUnifiedIdeographs})*"
 
@@ -35,6 +36,7 @@ Regex.Match("(abc)", firstIdentifier + identifier)
 Regex.Match("abc+def", firstIdentifier + identifier)
 Regex.Match("-abc", firstIdentifier + identifier)
 
+// 課題1: pExpressionとpProjectExpressionをここまでの仕様で完成させよ
 let pIdentifier = regex (firstIdentifier + identifier)
 test pIdentifier "@aaa"
 test pIdentifier "" // error
@@ -69,6 +71,7 @@ test pProjectExpression "project (project (シラバス) 専門, 学年, 場所)
 test pProjectExpression "project (project (aa) bb, cc, dd) bb, cc"
 test pProjectExpression "project (aa) [bb]]]]]]" // エラーになってほしい
 
+// 第一回と同じ感じのprojectでdfを返す所まで実装してみよう
 let rec evalExpression expression =
     match expression with
     | Identifier id -> Frame.ReadCsv ("database/master/" + id + ".csv")
@@ -89,6 +92,7 @@ let testProjectExpression str =
 testProjectExpression "project (Employee) Name, DeptName]"
 testProjectExpression "project (project (Employee) Name, EmpId, DeptName) Name, EmpId"
 
+// 課題2: dfのrowを一意にしたdf2を返す、distinctを作れ
 let distinct (df: Frame<int, string>) =
     df.RowsDense.Values |> Seq.distinct |> Series.ofValues |> Frame.ofRows
 
