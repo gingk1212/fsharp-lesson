4d0300bb432eb8137cf2dc36a9210c5f53e4e1b1
gingk <g.ingk.2927@gmail.com>
2022-08-06 23:34:05 +0900

    Add comment

--- sources/play_library/PlayDeedle/Scratch.fsx
+++ sources/play_library/PlayDeedle/Scratch.fsx
@@ -7,12 +7,16 @@ Frame.ReadCsv "../../data/シラバス.csv"
 
 let df = Frame.ReadCsv "../../data/シラバス.csv"
 
+// 課題2: 専門が数学の行だけを残そう
+// 課題3: 専門が数学の行だけを持ったFrameを作ろう
 df.RowsDense
 |> Series.filterValues(fun row -> row.GetAs<string>("専門") = "数学")
 |> Frame.ofRows
 
+// 課題4: 場所と学年だけのFrameを作ろう
 df.Columns.[ ["場所"; "学年"] ]
 
+// 課題5: フィルタとプロジェクションを関数にしよう
 let filter fn rows =
     rows |> Series.filterValues fn |> Frame.ofRows
 
