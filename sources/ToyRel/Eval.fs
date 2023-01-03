5ecf95569d3bfb8683507dc307cd3d9a7b9a51fe
gingk <g.ingk.2927@gmail.com>
2023-01-03 13:08:30 +0900

    Bring out functions modifyJoinExpCond and modifyBinOperand
    to improve testability

--- sources/ToyRel/Eval.fs
+++ sources/ToyRel/Eval.fs
@@ -57,90 +57,91 @@ and evalProductExpression expL expR =
         return rel
     }
 
-and evalJoinExpression joinExp =
-    // Modify BinOperand in condition as needed.
-    // For example, the left relation named 'Employee' has the following columns:
-    //   Name EmpId DeptName
-    // and the right relation named 'Dept' has the following columns:
-    //   DeptName Manager
-    // producting these relations, columns look like this:
-    //   Name EmpId DeptName Dept.DeptName Manager
-    // then, BinOperand in condition is modified as follows:
-    //   DeptName --> DeptName
-    //   Employee.DeptName --> DeptName
-    //   Dept.DeptName --> Dept.DeptName
-    //   Eemployee.Name --> Name
-    //   Dept.Manager --> Manager
-    let modifyJoinExpCond joinExp =
-        let getExpName exp =
-            match exp with
-            | Identifier (Identifier.Identifier str) -> str
-            | _ -> ""
+// join (expNameL) (expNameR) (...)
+and modifyBinOperand expNameL expNameR (expKeysL: Set<string>) (expKeysR: Set<string>) binOperand =
+    match binOperand with
+    | Column col ->
+        match col with
+        | PrefixedColumn ((Identifier.Identifier prefix), (Identifier.Identifier name)) ->
+            if (prefix = expNameL && expKeysL.Contains(name)) ||
+               (prefix = expNameR && not (expKeysL.Contains(name)) && expKeysR.Contains(name))
+            then
+                Result.Ok (Column(Column.Identifier(Identifier.Identifier(name))))
+            elif (prefix <> expNameL && prefix <> expNameR)
+            then
+                Result.Error (sprintf "No such prefix '%s'." prefix)
+            elif (prefix = expNameL && not (expKeysL.Contains(name))) ||
+                 (prefix = expNameR && not (expKeysR.Contains(name)))
+            then
+                Result.Error (sprintf "No such key '%s' in '%s'." name prefix)
+            else
+                Result.Ok (binOperand)
+        | _ ->
+            Result.Ok (binOperand)
+    | _ ->
+        Result.Ok (binOperand)
 
-        let getKeysFromExp exp =
-            evalExpression exp
-            |> Result.map (fun rel -> columnKeys rel |> set)
+// Modify BinOperand in JoinExpression condition as needed.
+// For example, the left relation named 'Employee' has the following columns:
+//   Name EmpId DeptName
+// and the right relation named 'Dept' has the following columns:
+//   DeptName Manager
+// producting these relations, columns look like this:
+//   Name EmpId DeptName Dept.DeptName Manager
+// then, BinOperand in condition is modified as follows:
+//   DeptName --> DeptName
+//   Employee.DeptName --> DeptName
+//   Dept.DeptName --> Dept.DeptName
+//   Eemployee.Name --> Name
+//   Dept.Manager --> Manager
+and modifyJoinExpCond joinExp =
+    let getExpName exp =
+        match exp with
+        | Identifier (Identifier.Identifier str) -> str
+        | _ -> ""
 
-        result {
-            let nameL = getExpName joinExp.ExpressionL
-            let nameR = getExpName joinExp.ExpressionR
-            let! keysL = getKeysFromExp joinExp.ExpressionL
-            let! keysR = getKeysFromExp joinExp.ExpressionR
+    let getKeysFromExp exp =
+        evalExpression exp
+        |> Result.map (fun rel -> columnKeys rel |> set)
 
-            let modifyBinOperand binOperand =
-                match binOperand with
-                | Column col ->
-                    match col with
-                    | PrefixedColumn ((Identifier.Identifier prefix), (Identifier.Identifier name)) ->
-                        if (prefix = nameL && keysL.Contains(name)) ||
-                           (prefix = nameR && not (keysL.Contains(name)) && keysR.Contains(name))
-                        then
-                            Result.Ok (Column(Column.Identifier(Identifier.Identifier(name))))
-                        elif (prefix <> nameL && prefix <> nameR)
-                        then
-                            Result.Error (sprintf "No such prefix '%s'." prefix)
-                        elif (prefix = nameL && not (keysL.Contains(name))) ||
-                             (prefix = nameR && not (keysR.Contains(name)))
-                        then
-                            Result.Error (sprintf "No such key '%s' in '%s'." name prefix)
-                        else
-                            Result.Ok (binOperand)
-                    | _ ->
-                        Result.Ok (binOperand)
-                | _ ->
-                    Result.Ok (binOperand)
+    result {
+        let nameL = getExpName joinExp.ExpressionL
+        let nameR = getExpName joinExp.ExpressionR
+        let! keysL = getKeysFromExp joinExp.ExpressionL
+        let! keysR = getKeysFromExp joinExp.ExpressionR
 
-            let modifyCondAtom condAtom =
+        let modifyCondAtom condAtom =
+            result {
+                let! l = modifyBinOperand nameL nameR keysL keysR condAtom.BinOperandL
+                let! r = modifyBinOperand nameL nameR keysL keysR condAtom.BinOperandR
+                return { condAtom with BinOperandL = l; BinOperandR = r }
+            }
+
+        let rec modifySingleCond singleCond =
+            match singleCond with
+            | Negation ng ->
+                modifySingleCond ng
+            | CondAtom ca ->
+                modifyCondAtom ca
+                |> Result.map CondAtom
+
+        let rec modifyCond cond =
+            match cond with
+            | InfixCondition ic ->
                 result {
-                    let! l = modifyBinOperand condAtom.BinOperandL
-                    let! r = modifyBinOperand condAtom.BinOperandR
-                    return { condAtom with BinOperandL = l; BinOperandR = r }
+                    let! newSc = modifySingleCond ic.SingleCondition
+                    let! newIc = modifyCond ic.Condition
+                    return InfixCondition({ ic with SingleCondition = newSc; Condition = newIc })
                 }
+            | SingleCondition sc ->
+                modifySingleCond sc
+                |> Result.map SingleCondition
 
-            let rec modifySingleCond singleCond =
-                match singleCond with
-                | Negation ng ->
-                    modifySingleCond ng
-                | CondAtom ca ->
-                    modifyCondAtom ca
-                    |> Result.map CondAtom
+        let! newCond = modifyCond joinExp.Condition
+        return newCond
+    }
 
-            let rec modifyCond cond =
-                match cond with
-                | InfixCondition ic ->
-                    result {
-                        let! newSc = modifySingleCond ic.SingleCondition
-                        let! newIc = modifyCond ic.Condition
-                        return InfixCondition({ ic with SingleCondition = newSc; Condition = newIc })
-                    }
-                | SingleCondition sc ->
-                    modifySingleCond sc
-                    |> Result.map SingleCondition
-
-            let! newCond = modifyCond joinExp.Condition
-            return newCond
-        }
-
+and evalJoinExpression joinExp =
     result {
         let! modifiedCond = modifyJoinExpCond joinExp
         let! productedRel = evalProductExpression joinExp.ExpressionL joinExp.ExpressionR
--- sources/ToyRel/Scratch.fsx
+++ sources/ToyRel/Scratch.fsx
@@ -39,170 +39,26 @@ changeDB (Identifier.Identifier "wikipedia")
 // Dept.Manager --> Manager
 
 
-//
-// getColumnType, getColumnValue にひと手間加える必要がある
-// 専用のカラムチェック用ヘルパー関数を用意するのがよさそう
-// チェックというより変換か。両関数ともにカラム名を使って目的の値を取得しているからカラム名を適切なものに変換する
-//
+let testCmd = parseCommand "join (Employee) (Dept) (not (Employee.DeptName=\"Finance\"))"
 
-// let test cmd colName =
-//     let (Relation df) =
-//         match testProductExpression cmd with
-//         | Result.Ok ok -> ok
-//         | Result.Error err -> failwithf "err: %s" err
-
-//     df.RowsDense.Values
-//     |> Seq.map (fun row ->
-//         let colVal = getColumnValue row colName
-//         colVal = "Harry")
-
-// --> 左右のrelation名が必要になるからやめる。getColumnTypeはrestrict (relation一つ) でも使うものだから。
-// --> 各カラムがどっちのrelに属していたかもわからないし。ここでは。
-
-
-//
-// 事前にConditionの中身を書き換えたものを作成する方針。Eval.fsでやるのがいいか
-//
-
-let cmd = parseCommand "restrict (Employee) (DeptName=\"Finance\")"
-// let cmd = parseCommand "restrict (Employee) (DeptName=\"Finance\" and DeptName=\"Sales\" and DeptName=\"Human Resources\")"
-
-let origCond =
-    match cmd with
-    | RestrictExpression re -> re.Condition
-    | _ -> failwith "fail"
-
-let origCondAtom =
-    match origCond with
-    | SingleCondition sc ->
-        match sc with
-        | CondAtom ca -> ca
-        | _ -> failwith "fail"
-    | _ -> failwith "fail"
-
-let condAtom =
-    { origCondAtom with
-        BinOperandL = Column(Column.Identifier(Identifier.Identifier "Employee.DepthName")) }
-
-let cond = SingleCondition(CondAtom(condAtom))
-
-
-//
-// Conditionを適切に書き換える関数を作ってみる。evalJoinExpressionの中で呼ばれる想定
-//
-
-let getExpName exp =
-    match exp with
-    | Identifier (Identifier.Identifier str) -> str
-    | _ -> ""
-
-let getKeysFromExp exp =
-    evalExpression exp
-    |> Result.map (fun rel -> columnKeys rel |> set)
-
-let modifyJoinExpCond joinExp =
-    result {
-        let nameL = getExpName joinExp.ExpressionL
-        let nameR = getExpName joinExp.ExpressionR
-        let! keysL = getKeysFromExp joinExp.ExpressionL
-        let! keysR = getKeysFromExp joinExp.ExpressionR
-
-        let modifyBinOperand binOperand =
-            match binOperand with
-            | Column col ->
-                match col with
-                | PrefixedColumn ((Identifier.Identifier prefix), (Identifier.Identifier name)) ->
-                    if (prefix = nameL && keysL.Contains(name)) ||
-                       (prefix = nameR && not (keysL.Contains(name)) && keysR.Contains(name))
-                    then
-                        Result.Ok (Column(Column.Identifier(Identifier.Identifier(name))))
-                    elif (prefix <> nameL && prefix <> nameR)
-                    then
-                        Result.Error (sprintf "No such prefix '%s'." prefix)
-                    elif (prefix = nameL && not (keysL.Contains(name))) ||
-                         (prefix = nameR && not (keysR.Contains(name)))
-                    then
-                        Result.Error (sprintf "No such key in '%s'." prefix)
-                    else
-                        Result.Ok (Column(Column.Identifier(Identifier.Identifier($"{prefix}.{name}"))))
-                | _ ->
-                    Result.Ok (binOperand)
-            | _ ->
-                Result.Ok (binOperand)
-
-        let modifyCondAtom condAtom =
-            result {
-                let! l = modifyBinOperand condAtom.BinOperandL
-                let! r = modifyBinOperand condAtom.BinOperandR
-                return { condAtom with BinOperandL = l; BinOperandR = r }
-            }
-
-        let rec modifySingleCond singleCond =
-            match singleCond with
-            | Negation ng ->
-                modifySingleCond ng
-            | CondAtom ca ->
-                modifyCondAtom ca
-                |> Result.map CondAtom
-
-        let rec modifyCond cond =
-            match cond with
-            | InfixCondition ic ->
-                result {
-                    let! newSc = modifySingleCond ic.SingleCondition
-                    let! newIc = modifyCond ic.Condition
-                    return InfixCondition({ ic with SingleCondition = newSc; Condition = newIc })
-                }
-            | SingleCondition sc ->
-                modifySingleCond sc
-                |> Result.map SingleCondition
-
-        let! newCond = modifyCond joinExp.Condition
-        return newCond
-    }
-
-
-// let testCmd = parseCommand "restrict (Employee) (Employee.DeptName=\"Finance\")"
-
-let testCmd =
-    parseCommand "restrict (Employee)
-        (Employee.DeptName=\"Finance\"
-         and Dept.DeptName=\"Sales\"
-         and DeptName=\"Human Resources\"
-         and Dept.Manager=\"George\")"
-        //  and Hoge.DeptName= \"Sales\")"
-
-// let testCmd =
-//     parseCommand "restrict (project (Employee) Name, EmpId, DeptName)
-//         (Dept.DeptName=\"Sales\"
-//          and DeptName=\"Human Resources\"
-//          and Dept.Manager=\"George\")"
-
-let testCond =
+let joinExp =
     match testCmd with
-    | RestrictExpression re -> re.Condition
+    | JoinExpression je -> je
     | _ -> failwith "fail"
 
-let joinExp = {
-    ExpressionL = Identifier(Identifier.Identifier("Employee"))
-    // ExpressionL =
-    //     match testCmd with
-    //     | RestrictExpression re -> re.Expression
-    //     | _ -> failwith "fail"
-    ExpressionR = Identifier(Identifier.Identifier("Dept"))
-    Condition = testCond
-}
+evalJoinExpression joinExp
 
 modifyJoinExpCond joinExp
 
 
-let evalJoinExpression joinExp =
-    result {
-        let! modifiedCond = modifyJoinExpCond joinExp
-        let! productedRel = evalProductExpression joinExp.ExpressionL joinExp.ExpressionR
-        let! rel = restrictOp modifiedCond productedRel
-        return rel
-    }
+let nameL = "Hoge"
+let nameR = "Fuga"
+let keysL = ["a"; "b"; "c"] |> set
+let keysR = ["c"; "d"; "e"] |> set
 
-evalJoinExpression joinExp
+let prefix = Identifier.Identifier("Hoge")
+let name = Identifier.Identifier("c")
+let binOperand = Column(PrefixedColumn(prefix, name))
+
+modifyBinOperand nameL nameR keysL keysR binOperand
 
